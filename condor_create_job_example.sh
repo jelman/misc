e34cb0443e84c6e05c9f7edc06e4a37365a5679c
jelman <jelman523@gmail.com>
2021-05-26 12:42:35 -0700

    condor examples

---
+++ condor_create_job_example.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#################################################################################################
+# Script to create indvidual condor jobs
+#
+################################################################################################
+
+
+# Set variables and paths
+DATA=/path/to/data
+OUTPUT=/path/to/output
+SCRIPTDIR=/path/to/scripts
+
+# Loop over all tissue models
+for SUBJECT in `cat path/to/subject_list.txt`
+do
+	
+cat > ${SCRIPTDIR}/run_${SUBJECT} << EOF
+#!/bin/bash
+
+# Set some variable within script
+N_FILES=`wc -l ${DATA}/$SUBJECT`
+
+/pathto/SomeCode.sh \
+--indir ${DATA}/${SUBJECT} 
+--num_files \$N_FILES
+--output $OUTPUT
+EOF
+done
+
+
